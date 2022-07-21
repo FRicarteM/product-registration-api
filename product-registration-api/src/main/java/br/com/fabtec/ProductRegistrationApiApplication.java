3f1e031731e56aa7079821a503b64a0be593797a
Fabricio Ricarte <fabricio_ricarte@hotmail.com>
2022-07-21 15:07:59 -0300

    Initial project commit

---
+++ product-registration-api/src/main/java/br/com/fabtec/ProductRegistrationApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabtec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRegistrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRegistrationApiApplication.class, args);
+	}
+
+}
---
+++ product-registration-api/src/test/java/br/com/fabtec/ProductRegistrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabtec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRegistrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
